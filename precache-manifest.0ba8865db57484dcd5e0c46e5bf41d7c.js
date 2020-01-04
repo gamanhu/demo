1b186f8f99c6eae70088fac6bc18664dd2459d9a
gamanhu <chrisnguyen995@gmail.com>
2020-01-04 15:20:24 +0700

    Updates

---
+++ precache-manifest.0ba8865db57484dcd5e0c46e5bf41d7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c753486771c8368a702ac0aa2b8370",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "2d2a042a3ed2bc958c54",
+    "url": "/demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4f6e88680ed722803367",
+    "url": "/demo/static/js/2.ea0eedf4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/demo/static/js/2.ea0eedf4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d2a042a3ed2bc958c54",
+    "url": "/demo/static/js/main.ad24ec38.chunk.js"
+  },
+  {
+    "revision": "7c10936abd05b9247c07",
+    "url": "/demo/static/js/runtime-main.c92e0419.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
